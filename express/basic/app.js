35a75faaec3414ac0f6df5c09767d818c5546aad
Sam <sammccathie2@gmail.com>
2022-04-05 12:30:24 +1200

    Serve static pages.

--- express/basic/app.js
+++ express/basic/app.js
@@ -1,12 +1,25 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 const PORT = 5000;
+const __dirname = path.resolve();
+
+// middleware = .use()
+
+// static asset is a file that the server does not have to change
+// - e.g. css, image and js
+// - saving in public file is common convention
+// - express also handles the content types
+app.use(express.static("./public"));
 
 // .get(path, callback)
 app.get("/", (req, res) => {
   //.status(status code) ->  you can chain actions
-  res.status(200).send("Homie");
+  // Note: 304 response -> 304 is not a problem. Your response is not modified and your browser turns to cache to fetch the resource.
+  res.status(200).sendFile(path.resolve(__dirname, "./index.html"));
+  // Note: The index file could be moved to the public file.
+  // - This way the "/" route would not need to be used.
 });
 
 app.get("/about", (req, res) => {
@@ -23,6 +36,7 @@ app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
 
+// types:
 // app.get
 // app.post
 // app.put
